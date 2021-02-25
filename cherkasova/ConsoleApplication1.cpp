898578069686423e231af415abd2d69d50914363
Ohh-uzh-eta-cherkasova <79604255+Ohh-uzh-eta-cherkasova@users.noreply.github.com>
2021-02-25 18:32:31 +0500

    Add files via upload

---
+++ cherkasova/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x;
+    setlocale(0, "");
+    cout << "введите х=";
+    cin >> x; 
+    if (x == 0 || x == sqrt(15) || x == -sqrt(15)) cout << "деление на ноль";
+    else cout << abs(pow(x, 2) - pow(x, 3)) - 7 * x / (pow(x, 3) - 15 * x);
+    return 0;
+}
